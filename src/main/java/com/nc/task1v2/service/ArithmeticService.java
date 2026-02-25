5016d848719d872bc881e3da8e327f41e6eb3ccb
Noel Cyriac <noel@gmail.com>
2026-02-25 14:35:38 +0530

    add arithmetic-endpoints

---
+++ src/main/java/com/nc/task1v2/service/ArithmeticService.java
@@ -0,0 +1,23 @@
+package com.nc.task1v2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArithmeticService {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
