fe442700653c3037a6b33baff99da99a4f8a96bd
Noel Cyriac <noel@gmail.com>
2026-02-25 11:07:40 +0530

    Initial setup

---
+++ src/main/java/com/nc/task1v2/Task1v2Application.java
@@ -0,0 +1,13 @@
+package com.nc.task1v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task1v2Application.class, args);
+	}
+
+}
